fd759c9abb65811da687d919dfa8f23288d94597
cVayssieres <cyril.vayssieres@gmail.com>
2015-03-30 16:11:02 +0200

    Des petits tests, des petits tests, partout des
    PETITS PUTAIN DE TESTS !!!!

--- test/testEvenement.js
+++ test/testEvenement.js
@@ -146,3 +146,95 @@ QUnit.test( "testEvenementGetColonne", function( assert ) {
 	
 	assert.equal(evmt.getColonne(),col,"Passed");
 });
+
+/*QUnit.test( "testEvenementMargeHaut", function( assert ) {
+	var periodeTest = new Periode({heureDeb:8,
+		minuteDeb : 30,
+		heureFin : 9,
+		minuteFin: 30});
+	var evmt=new EvenementClassique("TP","refactoring",periodeTest,"Informatique");
+	var col = new Colonne("blabla");
+	col.ajouterEvenement(evmt);
+	var planning = new Planning();
+	var page = new Page();
+	page.ajoutColonne(col);
+	planning._page.push(page);
+	alert(evmt.margeHaut(true));
+	
+	assert.equal(evmt.margeHaut(true),col,"Passed");
+});*/
+
+/*QUnit.test( "testSetNbEvenementSecondaire", function( assert ) {
+	var periodeTest = new Periode({heureDeb:8,
+		minuteDeb : 30,
+		heureFin : 9,
+		minuteFin: 30});
+	var evmt=new EvenementClassique("TP","refactoring",periodeTest,"Informatique",3);
+
+	var col = new Colonne("blabla");
+	var col2 = new Colonne("col2");
+	var col3 = new Colonne ("col3");
+	col.ajouterEvenement(evmt);
+	var planning = new Planning();
+	var page = new Page();
+	page.ajoutColonne(col);
+	page.ajoutColonne(col2);
+	page.ajoutColonne(col3);
+	planning._page.push(page);
+	evmt.setNbEvenementSecondaire(2);
+	var ev2 = evmt.getTabEvenementAutreCol()[0] = new EvenementInvisible();
+	var ev3 = evmt.getTabEvenementAutreCol()[1] = new EvenementInvisible();
+	col2.ajouterEvenement(ev2);
+	col2.ajouterEvenement(ev3);
+	
+	assert.deepEqual(col.getTaches()[0],evmt,"Passed");
+	assert.deepEqual(col2.getTaches()[0],ev2,"Passed");
+	assert.deepEqual(col2.getTaches()[0],ev3,"Passed");
+});*/
+
+QUnit.test( "testLargeur", function( assert ) {
+	var periodeTest = new Periode({heureDeb:8,
+		minuteDeb : 30,
+		heureFin : 9,
+		minuteFin: 30});
+	var evmt=new EvenementClassique("TP","refactoring",periodeTest,"Informatique",3);
+
+	var col = new Colonne("blabla");
+	var col2 = new Colonne("col2");
+	var col3 = new Colonne ("col3");
+	col.ajouterEvenement(evmt);
+	var planning = new Planning();
+	var page = new Page();
+	page.ajoutColonne(col);
+	page.ajoutColonne(col2);
+	page.ajoutColonne(col3);
+	planning._page.push(page);
+	assert.equal(evmt.largeur(),'100%',"Passed");
+});
+
+/*QUnit.test( "testSupprimer", function( assert ) {
+	var periodeTest = new Periode({heureDeb:8,
+		minuteDeb : 30,
+		heureFin : 9,
+		minuteFin: 30});
+	var planning = new Planning();
+	var page = new Page();
+	var evmt=new EvenementClassique("TP","refactoring",periodeTest,"Informatique",3);
+	evmt.getTabEvenementAutreCol().push(new EvenementInvisible());
+	var col = new Colonne("blabla");
+	var col2 = new Colonne("blabla");
+	col.ajouterEvenement(evmt);
+	col2.ajouterEvenement(new EvenementInvisible());
+	page.ajoutColonne(col);
+	page.ajoutColonne(col2);
+	planning._page.push(page);
+
+	assert.equal(col2.getTaches().length,1,"Passed");
+	assert.equal(evmt.getTabEvenementAutreCol().length,1,"Passed");
+	
+	evmt.getTabEvenementAutreCol()[0].supprimer();
+	
+	assert.equal(col.getTaches().length,0,"Passed");
+	assert.equal(evmt.getTabEvenementAutreCol().length,0,"Passed");
+});*/
+
